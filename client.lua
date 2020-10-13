9dd08c53f38a0cd8f0d47c64929481066090a07d
mefedroniny <71588818+mefedroniny@users.noreply.github.com>
2020-10-13 08:34:24 +0200

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,17 @@
+Citizen.CreateThread(function()
+	while true do
+		
+		SetDiscordAppId(762035981192790067) -- Client id for your bot
+		
+		SetDiscordRichPresenceAsset('example') -- Logo your server on discord developer portal
+		
+		SetDiscordRichPresenceAssetText('example') -- Text for your logo 
+				
+		local playerCount = #GetActivePlayers()
+        local playerName = GetPlayerName(PlayerId())
+		local maxPlayerSlots = "40"
+		
+		SetRichPresence(string.format("%s - %s/%s", playerName, playerCount, maxPlayerSlots))
+        Citizen.Wait(60000)
+    end
+end)
